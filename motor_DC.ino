0539c16e9921de290999650e46883751216b19a7
Dx78 <39838964+Dx78@users.noreply.github.com>
2018-06-03 17:15:28 -0600

    Add files via upload
    Programacion Del Robot Seguidor de Lineas

---
+++ motor_DC.ino
@@ -0,0 +1,38 @@
+int IN3 = 5; 
+int IN4 = 4;
+int IN2 = 3;
+int IN1 = 2;
+int BluetoothData;
+char unChar;
+
+void setup()
+{ Serial1.begin(9600); 
+  pinMode (IN4, OUTPUT);    // Input4 conectada al pin 4 
+  pinMode (IN3, OUTPUT);    // Input3 conectada al pin 5
+  pinMode (IN2, OUTPUT);    
+  pinMode (IN1, OUTPUT);
+}
+void loop()
+{ unChar=Serial1.read();
+  if (unChar == 'c'){
+  digitalWrite (IN4, HIGH);
+  digitalWrite (IN3, LOW);
+  digitalWrite (IN2, HIGH);
+  digitalWrite (IN1, LOW); 
+  delay(4000);}
+  // Motor no gira
+  if (unChar == 'd'){
+  digitalWrite (IN4, LOW);
+  digitalWrite (IN2, LOW); 
+  delay(500);}
+  // Motor gira en sentido inverso
+  if (unChar == 'e'){
+  digitalWrite (IN3, HIGH);
+  digitalWrite (IN1, HIGH);
+  delay(4000);}
+  // Motor no gira
+  if (unChar == 'f'){
+  digitalWrite (IN3, LOW);
+  digitalWrite (IN1, LOW); 
+  delay(5000);}
+}
